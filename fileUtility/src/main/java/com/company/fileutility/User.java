15527870f4b359cc3517f35fb5813ad6e0ccd75b
Nadir Nadirli <companybakuink@gmail.com>
2021-01-23 22:33:59 +0400

    for files

---
+++ fileUtility/src/main/java/com/company/fileutility/User.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.fileutilityoriginal;
+
+/**
+ *
+ * @author nnbak
+ */
+import java.io.Serializable;
+
+/**
+ *
+ * @author nnbak
+ */
+public class User implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    public String name;
+    
+    public transient String password;
+    
+    public int i;
+}
